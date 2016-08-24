05bea2563a92fc5d8447855af096b8581b4712f0
suzhida <jerome_s@qq.com>
2016-08-24 16:05:48 +0800

    新增各种请求的demo

--- src/main/java/com/demo/controller/TestController.java
+++ src/main/java/com/demo/controller/TestController.java
@@ -1,20 +1,73 @@
 package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.service.TestService;
 
 @RestController
-@RequestMapping("test/")
+@RequestMapping("test")
 public class TestController {
 
 	@Autowired
 	private TestService testService;
 
-	@RequestMapping("/test")
+	/**
+	 * 不区分get post
+	 * localhost/test
+	 * @author jerome_s@qq.com
+	 * @return
+	 */
+	@RequestMapping("")
 	String test() {
 		return testService.test();
 	}
+	
+	/**
+	 * Get请求
+	 * localhost/test/test?name=jerome
+	 * 
+	 * @author jerome_s@qq.com
+	 * @param name
+	 *            名字(默认必填)
+	 * @param age
+	 *            年龄(非必填)
+	 * @return
+	 */
+	@RequestMapping(value = "test", method = RequestMethod.GET)
+	String testGet(@RequestParam("name") String name, @RequestParam(value = "age", required = false) String age) {
+		return testService.test() + name;
+	}
+	
+	/**
+	 * Post请求
+	 * 
+	 * @author jerome_s@qq.com
+	 * @return
+	 */
+	@RequestMapping(value = "test", method = RequestMethod.POST)
+	String testPost() {
+		return testService.test();
+	}
+	
+	/**
+	 * Restful请求
+	 * localhost/test/test/jerome/23
+	 * 
+	 * @author jerome_s@qq.com
+	 * @param name
+	 *            名字(默认必填)
+	 * @param age
+	 *            年龄(非必填)
+	 * @return
+	 */
+	@RequestMapping("test/{name}/{age}")
+	String testRestful(@PathVariable String name, @PathVariable String age) {
+		return testService.test() + name;
+	}
+	
 }
